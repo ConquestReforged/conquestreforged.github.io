cd931de873abfdf0315b7617c67feb59ac4369df
CreativeRealms <creativerealmsmc@gmail.com>
2016-08-29 13:19:39 -0700

    Added Dev pack.

--- Launcher/packages.php
+++ Launcher/packages.php
@@ -1,29 +1,32 @@
 <?php
-// This file automatically generates packages.json
+$keys = isset($_GET['key']) ? array_map('trim', explode(',', strtolower($_GET['key']))) : array();
+$packages = array();
 
-$files = glob("*.json");
+$packages[] = array(
+    'name' => 'ConquestReforgedBasic',
+    'title' => 'Conquest Reforged (basic)',
+    'version' => '1.0.1',
+    'priority' => 0,
+    'location' => 'conquestreforgedbasic.json',
+);
 
-if ($files === false) {
-    $files = [];
-}
+$packages[] = array(
+    'name' => 'ConquestReforgedAdvanced',
+    'title' => 'Conquest Reforged (advanced)',
+    'version' => '1.0.1',
+    'priority' => 0,
+    'location' => 'conquestreforgedadvanced.json',
+);
 
-$document = [
-    'minimumVersion' => 1,
-    'packages' => [],
-];
+if (count(array_intersect(array('devbuild'), $keys)) > 0)
+$packages[] = array(
+    'name' => 'ConquestReforgedDev',
+    'title' => 'Conquest Reforged (dev)',
+    'version' => '1.0.1',
+    'priority' => 0,
+    'location' => 'conquestreforgeddev.json',
+);
 
-foreach ($files as $file) {
-    $data = json_decode(file_get_contents($file));
-    if (isset($data->name) && isset($data->version)) {
-        $document['packages'][] = [
-            'name' => $data->name,
-            'title' => isset($data->title) ? $data->title : $data->name,
-            'version' => $data->version,
-            'location' => basename($file),
-            'priority' => 1,
-        ];
-    }
-}
-
-header("Content-Type: text/plain");
-echo json_encode($document, JSON_PRETTY_PRINT);
+$out = array('minimumVersion' => 1, 'packages' => $packages);
+header('Content-Type: text/plain; charset=utf-8');
+echo json_encode($out);
